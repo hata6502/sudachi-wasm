1d587e0f0c1a6450d79256e5bec6adcb0ae0f97b
Tomoyuki Hata <7702653+hata6502@users.noreply.github.com>
2021-07-10 17:01:38 +0900

    Test wasm-bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,22 @@ pub mod tokenizer;
 
 pub mod dic;
 pub mod lattice;
+
+extern crate wasm_bindgen;
+
+use once_cell::sync::Lazy;
+use wasm_bindgen::prelude::*;
+use tokenizer::Mode;
+use tokenizer::Tokenizer;
+
+// embed dictionary binary file
+const BYTES: &[u8; 0] = include_bytes!("resources/system.dic");
+// load and parse dictionary binary to create a tokenizer
+const TOKENIZER: Lazy<Tokenizer> = Lazy::new(|| Tokenizer::new(BYTES));
+
+#[wasm_bindgen]
+pub fn tokenize() -> String {
+    let input = String::from("すもももももももものうち");
+
+    TOKENIZER.tokenize(&input, &Mode::C, false)[0].surface().clone()
+}
